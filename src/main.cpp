71a7bf4addf92efc01e85c22a7a8eeab7e52f9d6
Rogerio Junior <junior.rogerio8@gmail.com>
2021-09-02 02:35:36 -0300

    Componentize controlling logic inside Control class

--- src/main.cpp
+++ src/main.cpp
@@ -4,108 +4,36 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#include "display.hpp"
-#include "bme280.hpp"
-#include "uart.hpp"
-#include "pwm.hpp"
-#include "pid.hpp"
-
-#define DELAY 1000000
+#include "control.hpp"
 
 using namespace std;
 
+float potentiometer_temperature;
+float user_temperature = -100;
+float internal_temperature;
+int key_state;
+
 int main(int argc, char *argv[])
 {
-    float internal_temperature, reference_temperature;
-    char student_id[] = {1, 7, 5, 1};
+    Control *control = new Control();
+    // Display *display = new Display();
+    // display_message data;
 
-    UART *uart = new UART("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY, 0x01, student_id);
+    // data.message = "T: ";
+    // data.data = (const void *)&sensor_out->temperature;
+    // display->write_lcd(LINE1, FLOAT_MSG, &data);
 
-    Display *display = new Display();
-    display_message data;
+    // data.message = " P: ";
+    // data.data = (const void *)&sensor_out->pressure;
+    // display->write_lcd(0, FLOAT_MSG, &data);
 
-    BME280 *sensor = new BME280(1, 0x76);
-    sensor_data *sensor_out = sensor->get_data();
+    // data.message = "H:  ";
+    // data.data = (const void *)&sensor_out->humidity;
+    // display->write_lcd(LINE2, FLOAT_MSG, &data);
+    // data.message = "%";
+    // display->write_lcd(0, CHAR_MSG, &data);
 
-    PWM *resistor = new PWM(RESISTOR);
-    PWM *fan = new PWM(FAN);
-
-    PID *pid = new PID(5.0, 1.0, 5.0);
-
-    // resistor->send(100);
-    fan->send(100);
-    sleep(60);
-
-    data.message = "T: ";
-    data.data = (const void *)&sensor_out->temperature;
-    display->write_lcd(LINE1, FLOAT_MSG, &data);
-
-    data.message = " P: ";
-    data.data = (const void *)&sensor_out->pressure;
-    display->write_lcd(0, FLOAT_MSG, &data);
-
-    data.message = "H:  ";
-    data.data = (const void *)&sensor_out->humidity;
-    display->write_lcd(LINE2, FLOAT_MSG, &data);
-    data.message = "%";
-    display->write_lcd(0, CHAR_MSG, &data);
-
-    data_interface request, response;
-
-    request.code = READ;
-    request.sub_code = READ_TI;
-
-    uart->send_message(&request);
-    uart->read_message(&response);
-
-    cout << "\nTI:" << endl;
-    cout << (*((float *)response.data)) << endl;
-
-    internal_temperature = (*((float *)response.data));
-
-    free(response.data);
-
-    request.code = READ;
-    request.sub_code = READ_TR;
-
-    uart->send_message(&request);
-    uart->read_message(&response);
-
-    cout << "\nTR:" << endl;
-    cout << (*((float *)response.data)) << endl;
-
-    reference_temperature = (*((float *)response.data));
-
-    free(response.data);
-
-    request.code = READ;
-    request.sub_code = READ_STATE;
-
-    uart->send_message(&request);
-    uart->read_message(&response);
-
-    cout << "\nState:" << endl;
-    cout << (*((float *)response.data)) << endl;
-
-    free(response.data);
-
-    request.code = WRITE;
-    request.sub_code = WRITE_SIGNAL;
-
-    request.data = (int *)malloc(sizeof(int));
-
-    *((int *)request.data) = -21;
-
-    uart->send_message(&request);
-
-    free(request.data);
-
-    pid->set_reference(reference_temperature);
-
-    printf("PID: %.0lf\n", pid->get_pid(internal_temperature));
-
-    delete fan;
-    delete resistor;
+    control->go();
 
     return 0;
 }
